e9a9856dcc0895bbe6c148f02c356156c40745c9
AlbertoCamachoMunoz <c4max0@gmail.com>
2023-04-29 14:51:51 +0200

    usa selenium para scrappear web con javascript

--- services/services_web_browser.py
+++ services/services_web_browser.py
@@ -1,18 +1,56 @@
+# from googlesearch import search
+# import requests
+# from bs4 import BeautifulSoup
+# from operator import itemgetter
+#
+# class WebBrowserServices:
+#
+#     def open_browser(self, query):
+#         if query is not None:
+#             try:
+#                 for j in search(query, num_results=1):
+#                     soup = self.get_page_content(j)
+#                     if soup is not None:
+#                         page_content = ' '.join(soup.stripped_strings)
+#                         print(f" open_browser {page_content}")
+#                         limited_page_content = ' '.join(page_content.split())
+#                         return limited_page_content
+#
+#                     break
+#             except Exception as e:
+#                 print("Error al realizar la búsqueda:", e)
+#         return None
+#
+#
+#     def get_page_content(self, url):
+#         try:
+#             response = requests.get(url)
+#             response.raise_for_status()
+#             soup = BeautifulSoup(response.text, 'html.parser')
+#             return soup
+#         except requests.exceptions.HTTPError as e:
+#             print(f"Error al obtener la página: {e}")
+#             return None
+#
+#
+#
 from googlesearch import search
-import requests
-from bs4 import BeautifulSoup
-from operator import itemgetter
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
 
 class WebBrowserServices:
+
+    def __init__(self):
+        self.chrome_driver_path = "C:/webdriver/chromedriver.exe"
 
     def open_browser(self, query):
         if query is not None:
             try:
                 for j in search(query, num_results=1):
-                    soup = self.get_page_content(j)
-                    if soup is not None:
-                        page_content = ' '.join(soup.stripped_strings)
-                        print(f" open_browser {page_content}")
+                    page_content = self.get_page_content(j)
+                    if page_content is not None:
+                        print(f"open_browser {page_content}")
                         limited_page_content = ' '.join(page_content.split())
                         return limited_page_content
 
@@ -21,16 +59,15 @@ class WebBrowserServices:
                 print("Error al realizar la búsqueda:", e)
         return None
 
-
     def get_page_content(self, url):
         try:
-            response = requests.get(url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'html.parser')
-            return soup
-        except requests.exceptions.HTTPError as e:
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            driver = webdriver.Chrome(executable_path=self.chrome_driver_path, options=chrome_options)
+            driver.get(url)
+            page_content = driver.page_source
+            driver.quit()
+            return page_content
+        except Exception as e:
             print(f"Error al obtener la página: {e}")
             return None
-
-
-
